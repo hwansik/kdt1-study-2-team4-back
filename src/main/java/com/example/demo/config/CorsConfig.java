b5a5d3ae3eed644391c22d2b994e98071c791210
hwansik <hwansik08@gmail.com>
2023-06-05 15:31:21 +0900

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -10,18 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://127.0.0.1:8080",
-                        "http://localhost:8080",
-                        "http://3.38.61.219", // 다운
-                        "http://ec2-3-38-61-219.ap-northeast-2.compute.amazonaws.com/",
-                        "http://43.200.129.17", // 여울
-                        "http://ec2-43-200-129-17.ap-northeast-2.compute.amazonaws.com/",
-                        "http://43.202.26.168", // 주아
-                        "http://ec2-43-202-26-168.ap-northeast-2.compute.amazonaws.com/",
-                        "http://3.36.13.137",
-                        "http://ec2-3-36-13-137.ap-northeast-2.compute.amazonaws.com")
-
+                .allowedOrigins("http://3.36.13.137")
+                .allowedOrigins("ec2-3-36-13-137.ap-northeast-2.compute.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
